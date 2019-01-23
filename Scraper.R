5748520be29df4e73f6178b6c6c50d2784d0916d
Alok Madan <35575635+amadan95@users.noreply.github.com>
2019-01-22 22:38:00 -0500

    Update Scraper.R

--- Scraper.R
+++ Scraper.R
@@ -13,64 +13,6 @@ library(stringr)
 ID <- #######
 
 
-Date <- lapply(paste0('http://www.imdb.com/title/tt', ID, '/reviews?filter=prolific', 1:10),
-                function(url){
-                  url %>% read_html() %>% 
-                    html_nodes(".review-date") %>% 
-                    html_text() %>%
-                    gsub('[\r\n\t]', '', .)
-                  
-                })
-
-Stars <- lapply(paste0('http://www.imdb.com/title/tt', ID, '/reviews?filter=prolific', 1:10),
-                function(url){
-                  url %>% read_html() %>% 
-                    html_nodes(".rating-other-user-rating") %>% 
-                    html_text() %>%
-                    gsub('[\r\n\t]', '', .)
-                  
-                })
-
-Title <- lapply(paste0('http://www.imdb.com/title/tt', ID, '/reviews?filter=prolific', 1:10),
-                function(url){
-                  url %>% read_html() %>% 
-                    html_nodes(".title") %>% 
-                    html_text() %>%
-                    gsub('[\r\n\t]', '', .)
-                  
-                })
-
-
-
-Review <- lapply(paste0('http://www.imdb.com/title/tt', ID, '/reviews?filter=prolific', 1:10),
-                  function(url){
-                    url %>% read_html() %>% 
-                      html_nodes(".show-more__control") %>% 
-                      html_text() %>%
-                      gsub('[\r\n\t]', '', .)
-                    
-                  })
-
-#Convert to Array
-
-Date <- unlist(Date)
-Title <- unlist(Title)
-Stars <- unlist(Stars)
-Review <- unlist(Review)
-
-merge <- cbind(Date,Title,Stars,Review)
-
-
-#Covert to Data Frame
-Final_Table <- data.frame(merge,stringsAsFactors = FALSE)
-
-#View Table
-View(Final_Table)
-
-#Export as CSV
-write.csv(Final_Table,file='IMDB_Reviews.csv')
-
-
 
 IMDB.All <- lapply(paste0('http://www.imdb.com/title/tt', ID, '/reviews?filter=prolific', 1:10),
                     function(url){
@@ -83,7 +25,8 @@ IMDB.All <- lapply(paste0('http://www.imdb.com/title/tt', ID, '/reviews?filter=p
 IMDB_Array <- unlist(IMDB.All)
 
 
+#Export as CSV
+write.csv(Final_Table,file='IMDB_Reviews.csv')
 
-r.1<- as.data.frame(split(IMDB_Array, 1:4))
 
 
